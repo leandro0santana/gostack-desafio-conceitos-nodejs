f286dab45f271b4ab6be28a650edc71458732d6e
Leandro C. Santana <leandro.c.santana@hotmail.com>
2020-06-16 15:35:49 -0300

    Route for creating repositories

---
+++ src/app.js
@@ -0,0 +1,40 @@
+const express = require("express");
+const cors = require("cors");
+
+const { uuid } = require("uuidv4");
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+const repositories = [];
+
+app.get("/repositories", (request, response) => {
+  // TODO
+});
+
+app.post("/repositories", (request, response) => {
+  const { title, url, techs } = request.body;
+  const links = 0;
+
+  const repository = { id: uuid(), title, url, techs, links };
+
+  repositories.push(repository);
+
+  return response.json(repository);
+});
+
+app.put("/repositories/:id", (request, response) => {
+  // TODO
+});
+
+app.delete("/repositories/:id", (request, response) => {
+  // TODO
+});
+
+app.post("/repositories/:id/like", (request, response) => {
+  // TODO
+});
+
+module.exports = app;
